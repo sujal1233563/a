9ffe14c578aa44b56780db9d937cab6fb7d43a87
sujal1233563 <telegramuse001q@gmail.com>
2025-11-04 23:29:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # Admin configuration
-ADMIN_IDS = [7793257011]  # Add your admin IDs here
+ADMIN_IDS = [8383373235]  # Add your admin IDs here
 
 # MongoDB configuration
 MONGODB_URI = "mongodb+srv://dhruvkachhiya75622:c4gb6msfE0jzGtFa@cluster0.6e8wrs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+
 
